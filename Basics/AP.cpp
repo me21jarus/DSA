673900fb59515aa6e456ec618ead70596196e0da
me21jarus <134506803+me21jarus@users.noreply.github.com>
2026-02-14 06:52:14 +0000

    Organised Files

---
+++ Basics/AP.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int ans;
+
+    int a,d,n;
+    cin>> a >> d >> n;
+
+    int ap = a + n*d;
+
+    cout<<ap<<endl;
+
+    return 0;
+}
