9d30e00cbdfdaf2e96130f18dcbe22d2761341fe
me21jarus <134506803+me21jarus@users.noreply.github.com>
2026-04-16 09:47:16 +0000

    Merge2BST

--- Tree/Merge2BST.cpp
+++ Tree/Merge2BST.cpp
@@ -29,8 +29,96 @@ class Solution {
         
         Node* root=new Node(inorder[mid]);
         root->left = vectorToBST(s,mid-1,inorder);
-        root->left = vectorToBST(mid+1,e,inorder);
+        root->right = vectorToBST(mid+1,e,inorder);
         
+        return root;
+    }
+    
+    void convertIntoSortedDLL(Node* root, Node* &head){
+        if(root==NULL) return;
+        
+        convertIntoSortedDLL(root->right, head);
+        
+        root->right = head;
+        if(head!=NULL){
+            head->left = root;
+        }
+        head = root;
+        
+        convertIntoSortedDLL(root->left, head);
+    }
+    
+    Node* merge2SortedLL(Node* head1, Node* head2){
+        Node* head = NULL;
+        Node* tail = NULL;
+        
+        while(head1!=NULL && head2!=NULL){
+            if(head1->data < head2->data){
+                if(head == NULL){
+                    head = head1;
+                    tail = head1;
+                    head1 = head1->right;
+                    head->left = NULL;
+                }
+                else{
+                    tail->right = head1;
+                    head1->left = tail;
+                    tail = head1;
+                    head1 = head1->right;
+                }
+            }
+            else{
+                if(head == NULL){
+                    head = head2;
+                    tail = head2;
+                    head2 = head2->right;
+                    head->left = NULL;
+                }
+                else{
+                    tail->right = head2;
+                    head2->left = tail;
+                    tail = head2;
+                    head2 = head2->right;
+                }
+            }
+        }
+        
+        while(head1!=NULL){
+            tail->right = head1;
+            head1->left = tail;
+            tail = head1;
+            head1 = head1->right;
+        }
+        while(head2!=NULL){
+            tail->right = head2;
+            head2->left = tail;
+            tail = head2;
+            head2 = head2->right;
+        }
+        return head;
+    }
+    
+    int count(Node* root){
+        int i = 0;
+        Node*temp = root;
+        while(temp){
+            i++;
+            temp = temp->right;
+        }
+        return i;
+    }
+    
+    Node* sortedLLtoBST(Node* &head,int n){
+        if(head==NULL || n<=0){
+            return NULL;
+        }
+        Node* left = sortedLLtoBST(head, n/2);
+        Node* root = head;
+        root->left = left;
+        
+        head = head->right;
+        
+        root->right = sortedLLtoBST(head, n-n/2-1);
         return root;
     }
     
@@ -64,6 +152,18 @@ class Solution {
         inorder(root1, inorder1);
         inorder(root2, inorder2);
         
+        
+        Node* head1 = NULL;
+        Node* head2 = NULL;
+        convertIntoSortedDLL(root1,head1);
+        convertIntoSortedDLL(root2, head2);
+        if(head1) head1->left = NULL;
+        if(head2) head2->left = NULL;
+        
+        
+        Node* head = merge2SortedLL(head1,head2);
+        int n = count(head);
         return merge2Arrays(inorder1,inorder2);
+        // return sortedLLtoBST(head, n);
     }
 };
